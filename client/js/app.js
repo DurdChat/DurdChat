af4b7c5af1e144c109bb4035206ba54d5bc4494b
durdchat <durd.chat@gmail.com>
2015-07-20 19:49:36 +0000

    Needs reconnect button, talk button implementation, and better design.

--- client/js/app.js
+++ client/js/app.js
@@ -1,5 +1,131 @@
-var io = require('socket.io-client');
+var socket = io();
+var connected = false;
+var rating;
+var partnerRating;
+var isRating;
 
 $(document).ready(function() {
-    var socket = io();
+    
+    updateRating();
+    socket.on('partnerFound', function(pr) {
+        connected = true;
+        isRating = false;
+        partnerRating = pr;
+        clearMessages();
+        displaySystemMessage('You are connected!');
+    });
+    socket.on('sendMessage', function(msg) {
+        displayOtherMessage(msg);
+    });
+    socket.on('partnerDisconnected', function() {
+        displaySystemMessage('The other person has disconnected.');
+        displaySystemMessage('Please rate the other person on a scale from -5 to 5.');
+        isRating = true;
+        connected = false;
+    });
+    
+    $('#logo').on('click', function() {
+        updateRating();
+        clearMessages();
+        
+        $('#intro-page').show();
+        $('#chat-page').hide();
+        
+        socket.emit('removeFromWaitinglist');
+        console.log('Sent waitinglist removal request.');
+        if(connected) {
+            socket.emit('chatDisconnect');
+        }
+        
+        connected = false;
+        isRating = false;
+    });
+    
+    $('#roleplay-start').on('click', function() {
+        $('#intro-page').hide();
+        $('#chat-page').show();
+        isRating = false;
+        
+        socket.emit('onRPWaiting');
+        displaySystemMessage('Searching for other...');
+    });
+    
+    $('#talk-start').on('click', function() {
+        $('#intro-page').hide();
+        $('#chat-page').show();
+        isRating = false;
+        
+        socket.emit('onTalkWaiting');
+        displaySystemMessage('Searching for other...');
+    });
+    
+    $("#msg-input").keydown(function(event){
+        if(event.keyCode == 13) {
+            if(connected && $('#msg-input').val().length !== 0) {
+                displayOwnMessage($('#msg-input').val());
+                socket.emit('sendMessage', $('#msg-input').val());
+                $('#msg-input').val('');
+            } else if(isRating) {
+                var input = parseInt($('#msg-input').val(), 10);
+                
+                if(validRating(input)) {
+                    displaySystemMessage('Your rating has been sent.');
+                    socket.emit('sendRating', input);
+                    $('#msg-input').val('');
+                    isRating = false;
+                } else {
+                    displaySystemMessage('Must be a number from -5 to 5.');
+                }
+            }
+        }
+    });
+    
+    $('#disconnect').on('click', function() {
+        if(connected) {
+            onDisconnect();
+            connected = false;
+        }
+    });
 });
+
+function scrollDown() {
+    $("#chat-display").scrollTop($("#chat-display")[0].scrollHeight);
+}
+
+function displayOwnMessage(message) {
+    $('#chat-display').append('<p class="message"><span class="you">You (' + rating + '):</span> ' + message + '</p><br>');
+    scrollDown();
+}
+
+function displayOtherMessage(message) {
+    $('#chat-display').append('<p class="message"><span class="other">Other (' + partnerRating + '):</span> ' + message + '</p><br>');
+    scrollDown();
+}
+
+function displaySystemMessage(message) {
+    $('#chat-display').append('<p class="message" style="font-weight: bold;">' + message +'</p><br>');
+    scrollDown();
+}
+
+function onDisconnect() {
+    socket.emit('chatDisconnect');
+    isRating = true;
+    displaySystemMessage('You left the chat!');
+    displaySystemMessage('Please rate the other person on a scale from -5 to 5.');
+}
+
+function clearMessages() {
+    $('#chat-display').empty();
+}
+
+function updateRating() {
+    socket.emit('getRating');
+    socket.on('sendRating', function(r) {
+        $('#rating').text(r);
+        rating = r;
+    });
+}
+
+function validRating(input) {
+    return input === parseInt(input, 10) && input >= -5 && input <= 5;
+}
--- server.js
+++ server.js
@@ -1,22 +1,106 @@
+var socket_io = require('socket.io');
 var http = require('http');
 var express = require('express');
-var socket_io = require('socket.io');
 
 var app = express();
-app.use(express.static(__dirname + '/../client'));
+app.use(express.static('client'));
 
 var server = http.Server(app);
 var io = socket_io(server);
 
-var config  = require('./config.json');
-var sockets = [];
+var rpWaitinglist = [];
+var talkWaitinglist = [];
 
 io.on('connection', function (socket) {
-    sockets.push(socket);
-    sockets.set('rating', 0, function() {
-      console.log('New user with rating 0.');
+    socket.rating = 20;
+    socket.partner = null;
+    console.log('New user with rating: ' + socket.rating);
+    console.log('RP Waiting: ' + rpWaitinglist.length);
+    console.log('Talk Waiting: ' + talkWaitinglist.length);
+    
+    socket.on('getRating', function() {
+      socket.emit('sendRating', socket.rating);
+    });
+    
+    socket.on('onRPWaiting', function() {
+      var minDifference = -1;
+      var index = -1;
+      for(var i = 0; i < rpWaitinglist.length; i++) {
+        var difference = Math.abs(rpWaitinglist[i].rating - socket.rating);
+        if((minDifference === -1 || difference <= minDifference) && difference <= 10) {
+          minDifference = difference;
+          index = i;
+        }
+        
+        //No partner found.
+        if(index === -1) {
+          console.log('No partner found. User added to RP waitinglist.');
+          rpWaitinglist.push(socket);
+        } else {
+          console.log('Pairing partners.');
+          //Pair partners.
+          var partner = rpWaitinglist[index];
+          //Remove partner from RP waitinglist.
+          rpWaitinglist.splice(index, 1);
+          socket.partner = partner;
+          partner.partner = socket;
+          
+          socket.emit('partnerFound', partner.rating);
+          partner.emit('partnerFound', socket.rating);
+        }
+      }
+      
+      rpWaitinglist.push(socket);
+    });
+    
+    socket.on('onTalkWaiting', function() {
+      talkWaitinglist.push(socket);
+    });
+    
+    socket.on('disconnect', function() {
+      removeFromWaitinglist(socket);
+      console.log('User exited and removed from waitinglist.');
+      
+      if(socket.partner !== null)
+        socket.partner.emit('partnerDisconnected');
+    });
+    
+    socket.on('removeFromWaitinglist', function() {
+      removeFromWaitinglist(socket);
+      console.log('Received removal request.');
+    });
+    
+    socket.on('sendMessage', function(msg) {
+      socket.partner.emit('sendMessage', msg);
+    });
+    
+    socket.on('chatDisconnect', function() {
+      socket.partner.emit('partnerDisconnected');
+    });
+    
+    socket.on('sendRating', function(rating) {
+      socket.partner.rating += rating;
+      socket.partner = null;
     });
 });
 
-var serverPort = process.env.PORT || config.port;
-server.listen(serverPort);
+function removeFromWaitinglist(socket) {
+  console.log('Removed from waitinglist.');
+  console.log('RP Waiting: ' + rpWaitinglist.length);
+  console.log('Talk Waiting: ' + talkWaitinglist.length);
+  
+  for(var i = 0; i < rpWaitinglist.length; i++) {
+    if(rpWaitinglist[i].id === socket.id) {
+      rpWaitinglist.splice(i, 1);
+    }
+  }
+  for(i = 0; i < talkWaitinglist.length; i++) {
+    if(talkWaitinglist[i].id === socket.id) {
+      talkWaitinglist.splice(i, 1);
+    }
+  }
+}
+
+var serverPort = process.env.PORT;
+var serverHost = process.env.IP;
+server.listen(serverPort, serverHost);
